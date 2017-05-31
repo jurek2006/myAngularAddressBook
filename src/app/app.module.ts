2cb8aef8616e829e67eeb5c9bde937c191264c09
Jurek <jurek2006@gmail.com>
2017-05-31 19:45:36 +0200

    Stworzenie struktory komponentów

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactElementComponent } from './contact-list/contact-element/contact-element.component';
+import { ContactListPanelComponent } from './contact-list/contact-list-panel/contact-list-panel.component';
+import { ContactDisplayComponent } from './contact-display/contact-display.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactElementComponent,
+    ContactListPanelComponent,
+    ContactDisplayComponent,
+    ContactEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact-list/contact-element/contact-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-element',
+  templateUrl: './contact-element.component.html',
+  styleUrls: ['./contact-element.component.css']
+})
+export class ContactElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
