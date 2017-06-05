32f122f81741cd2a5dee3fc76506ef7bcddb688f
Jurek <jurek2006@gmail.com>
2017-06-05 20:08:34 +0200

    Dodanie stylu bootstrap i podstawowa struktura projektu

---
+++ src/app/contact-list/contact.model.ts
@@ -0,0 +1,10 @@
+// Model kontaktu
+export class Contact{
+	constructor(
+		public nazwisko: string,
+		public imie: string,
+		public drugieImie: string,
+		public nrTelefonu: string,
+		public email: string
+	){}
+}
